4ee52b16c4de49ace8eb3134aabd69a83a968897
sonyvakode <sonyvakode648@gmail.com>
2025-09-22 19:53:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Explicitly externalize main.tsx so Netlify doesn’t choke
-      external: ['/src/main.tsx'],
-    },
-  },
-})
+  base: './', // Fix for Netlify blank page
+});
